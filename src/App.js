2af6b4f5bb3a2064f1590d2d6ed4f978bdb5becf
MathTH <mathdhferreira@gmail.com>
2022-08-22 09:08:20 -0300

    Passando o arquivo para JSX

--- src/App.js
+++ src/App.js
@@ -1,99 +1,13 @@
-import React, { useRef, useEffect, useState } from 'react';
+import React from 'react';
+import Camera from './Components/Camera';
 
 
-function App() {
-  const videoRef = useRef(null);
-  const photoRef = useRef(null);
-
-  const [hasPhoto, setHasPhoto] = useState(false);
-
-
-  const getVideo = () => {
-    navigator.mediaDevices.getUserMedia({ video: { width: 1700, height: 1080} })
-      .then(stream => {
-        let video = videoRef.current;
-        video.srcObject = stream;
-        video.play();
-
-        //videoRef.current.srcObject = stream;
-        //videoRef.current.play();
-      })
-      .catch(err => console.error(err));
-  };
-
-  const takePhoto = () => {
-    const width = 1920;
-    const height = width / (16 / 9);
-
-    let video = videoRef.current;
-    let photo = photoRef.current;
-
-    photo.width = width;
-    photo.height = height;
-
-    let ctx = photo.getContext('2d');
-    ctx.drawImage(video, 0, 0, width, height);
-    setHasPhoto(true);
-
-  }
-
-  const BaixarPhoto = () => {
-  const photo = photoRef.current;
-  const link = document.createElement('a');
-  
-
-
-
-
-  link.href = photo.toDataURL('/image/png');
-
-  link.download = 'Documentos-Scanner.png';
-  link.click();
-
-  
-
-
-  
-
-
-
-
-
-
-
-
-
-  };
-
-
-  
-
-  const closePhoto = () => {
-  let photo = photoRef.current;
-  let ctx = photo.getContext('2d');
-
-  ctx.clearRect(0, 0, photo.width, photo.height);
-   setHasPhoto(true);
-  }
-
-  useEffect(() => {
-    getVideo();
-  }, [videoRef]);
-
-
-  return (
-    <div className="App">
-      <div className="camera">
-        <video ref={videoRef}></video>
-        <button onClick={takePhoto}>CAPTURAR FOTO</button>
-      </div>
-      <div className={'result' + (hasPhoto ? 'hasPhoto' : '')}>
-        <canvas ref={photoRef}></canvas>
-        <button onClick={BaixarPhoto}>BAIXAR FOTO</button>
-      </div>
-      
-    </div>
-  );
+function App () {
+  return(
+    <>
+    <Camera/>
+    </>
+  )
 }
 
 export default App;
---
+++ src/Components/Camera.jsx
@@ -0,0 +1,92 @@
+import React, { useRef, useEffect, useState } from 'react';
+
+function Camera() {
+    const videoRef = useRef(null);
+    const photoRef = useRef(null);
+
+    const [hasPhoto, setHasPhoto] = useState(false);
+
+
+    const getVideo = () => {
+        navigator.mediaDevices.getUserMedia({ video: { width: 1700, height: 1080 } })
+            .then(stream => {
+                let video = videoRef.current;
+                video.srcObject = stream;
+                video.play();
+
+                //videoRef.current.srcObject = stream;
+                //videoRef.current.play();
+            })
+            .catch(err => console.error(err));
+    };
+
+    const takePhoto = () => {
+        const width = 1920;
+        const height = width / (16 / 9);
+
+        let video = videoRef.current;
+        let photo = photoRef.current;
+
+        photo.width = width;
+        photo.height = height;
+
+        let ctx = photo.getContext('2d');
+        ctx.drawImage(video, 0, 0, width, height);
+        setHasPhoto(true);
+
+    }
+
+    const BaixarPhoto = () => {
+        const photo = photoRef.current;
+        const link = document.createElement('a');
+
+
+
+
+
+        link.href = photo.toDataURL('/image/png');
+
+        link.download = 'Documentos-Scanner.png';
+        link.click();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    };
+
+
+
+
+
+
+    useEffect(() => {
+        getVideo();
+    }, [videoRef]);
+
+
+    return (
+        <div className="App">
+            <div className="camera">
+                <video ref={videoRef}></video>
+                <button onClick={takePhoto}>CAPTURAR FOTO</button>
+            </div>
+            <div className={'result' + (hasPhoto ? 'hasPhoto' : '')}>
+                <canvas ref={photoRef}></canvas>
+                <button onClick={BaixarPhoto}>BAIXAR FOTO</button>
+            </div>
+
+        </div>
+    );
+}
+
+export default Camera;
